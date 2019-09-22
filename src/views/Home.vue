791d2484aad74a571b26f4b63d7aaa43f5f36755
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2019-09-22 20:46:10 +0200

    Added SkillTree to Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="home">
-    Hi there!
+    <section class="sectionSkills">
+      <h1 class="header">Skills</h1>
+      <skill-tree class="skillTree" />
+    </section>
   </div>
 </template>
 
 <script>
-export default {};
+import SkillTree from '@/components/SkillTree.vue';
+
+export default {
+  components: {
+    SkillTree,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .header {
+    font-weight: bolder;
+    font-size: 42px;
+    text-align: center;
+  }
+
+  .skillTree {
+    text-align: center;
+    padding: 0 10%;
+  }
+}
+</style>
