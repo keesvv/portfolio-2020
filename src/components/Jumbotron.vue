34b49fc6a455195361ab023770edac3360e99090
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-15 20:28:43 +0100

    Completely redesigned Jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="jumbotron">
     <div class="content">
-      <img src="@/assets/profile.jpg" class="profilePicture">
       <h1 class="header">
-        <span class="blue">console</span>.<span
-              class="green">log</span>('<span
-              class="black">Hi there! My name is <strong>Kees van Voorthuizen</strong></span>');
-        <span class="cursor"></span>
+        Hi!
       </h1>
-      <div class="bio">
-        Hi there! My name is Kees van Voorthuizen — I'm a 16-years-old Dutch programmer,
-        full stack web developer and CTO & co-founder of <a href="https://tribecamp.com">Tribecamp</a>. I'm a huge fan of modern web frameworks,
-        and I like learning new things. I have 4-5 years of experience with C#.NET,
-        but nowadays I'm really into web development and creating scalable backends with Node.js.
-      </div>
+      <h2 class="subheader">
+        My name is Kees van Voorthuizen — I'm a 16-years-old Dutch programmer,
+        full stack web developer and CTO & Co-founder of <a href="https://tribecamp.com">Tribecamp</a>.
+      </h2>
+      <img src="@/assets/profile.jpg" class="profilePicture">
     </div>
   </div>
 </template>
@@ -34,49 +29,26 @@ export default {};
 }
 
 .jumbotron {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: rgb(235, 235, 235);
-  padding: 150px 20px;
-  text-align: center;
+  background: rgb(57, 0, 104);
+  color: white;
+  padding: 160px 100px;
+
+  .header {
+    font-size: 80px;
+    font-weight: lighter;
+  }
+
+  .subheader {
+    font-size: 35px;
+    width: 500px;
+    font-weight: lighter;
+    margin-bottom: 60px;
+  }
 
   .profilePicture {
     border-radius: 50%;
     width: 150px;
     margin-bottom: 15px;
-  }
-
-  .header {
-    color: rgb(140, 140, 140);
-    font-weight: normal;
-    font-family: monospace;
-
-    .blue {
-      color: rgb(23, 133, 235);
-    }
-
-    .green {
-      color: rgb(31, 167, 31);
-    }
-
-    .black {
-      color: black;
-    }
-  }
-
-  .bio {
-    width: 80%;
-    max-width: 1100px;
-    font-size: 20px;
-    margin: 25px auto 0;
-  }
-
-  .cursor {
-    margin-left: 8px;
-    background: black;
-    padding: 0 8px;
-    animation: pulse 0.6s alternate infinite linear;
   }
 }
 </style>
