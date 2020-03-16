2e66871fb7a05f10b5a4da74a3d693dfdbeee301
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-16 13:52:26 +0100

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+npm install
+npm build
+cd dist
+
+git init
+git config --local user.email "actions@github.com"
+git config --local user.name "GitHub Actions"
+
+git add -A
+git commit -m 'Deploy 🚀'
