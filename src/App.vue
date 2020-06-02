09cbe54b47566214430b61e1d281d9dd13042e39
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-06-02 13:38:29 +0200

    Use new Google Fonts embed format

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Nunito:300,400&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Poppins:500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
 html,
 body {
