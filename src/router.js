ac702ac34ab8a80ef1f47e9c89faea9fbfa2231e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-06-04 10:26:25 +0200

    Add Projects page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Skills from './views/Skills.vue';
+import Projects from './views/Projects.vue';
 
 Vue.use(Router);
 
@@ -10,8 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Skills',
+      component: Skills,
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: Projects,
     },
   ],
 });
---
+++ src/views/Projects.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="projects">
+    <span class="placeholder">
+      // TODO:
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.projects {
+  text-align: center;
+
+  .placeholder {
+    font-size: 22px;
+    padding: 50px 0;
+    color: gray;
+  }
+}
+</style>
