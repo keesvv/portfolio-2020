d64e0f71e34d2dc7fdb9c559321fd7ca2528785e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-09-25 09:27:00 +0200

    Hardcode 'About' content instead of fetching GitHub README

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="about container" v-html="about"></div>
+  <div class="about container">
+    <p>
+      My name is Kees van Voorthuizen — I'm a 17-years-old Dutch full stack developer
+      and CTO & Co-Founder of <a href="https://tribecamp.com">Tribecamp</a>. I'm a huge
+      fan of modern web frameworks, and I like learning new things. I have 4-5 years of
+      experience with C#.NET, but nowadays I'm mainly into web development and creating
+      scalable backends with Node.js and TypeScript.
+    </p>
+
+    <p>
+      I absolutely love working with GNU/Linux environments. I'm using Artix Linux with the
+      <a href="https://github.com/baskerville/bspwm">bspwm</a> window manager at the moment
+      and I've dealt with Debian, Ubuntu, Arch, Manjaro and Mint in the past. Vim and Visual
+      Studio Code are my preferred text editors.
+    </p>
+
+    <p>
+      My first ever experience with Linux was around the age of ten. At the time I got an
+      old computer with an empty hard disk from my aunt, on which I then installed Ubuntu
+      with LXDE on the advice of an acquaintance. I have become addicted to computers,
+      programming and tech ever since.
+    </p>
+
+    <p>
+      One of my future goals is to learn and fully understand the C language (and C++).
+      I'm also very interested in the Rust language and I'm quite familiar with Java and
+      Python.
+    </p>
+
+    <p>
+      Feel free to reach out! 🤓
+    </p>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      about: '',
-    };
-  },
-  methods: {
-    getAbout() {
-      fetch('https://raw.githubusercontent.com/keesvv/keesvv/master/README.md')
-        .then(fetchResult => fetchResult.body.getReader().read())
-        .then((result) => {
-          // eslint-disable-next-line no-undef
-          const converter = new showdown.Converter();
-          const rawContent = new TextDecoder('utf-8')
-            .decode(result.value)
-            .split('\n');
-
-          rawContent.splice(0, 3);
-
-          this.about = converter.makeHtml(rawContent.join('\n'));
-        });
-    },
-  },
-  created() {
-    this.getAbout();
-  },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
